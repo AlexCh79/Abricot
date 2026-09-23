d4f3ec02c7ddd3f2791349b1198f4802d37287c9
Alexandra Chanteloup <275184320+AlexCh79@users.noreply.github.com>
2026-09-23 14:28:55 +0200

    Redirection de la page d'accueil du site vers la page de connexion

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Page de présentation</h1>
-      <p>Site en construction</p>
-    </div>
-  );
+  redirect("/login");
 }
