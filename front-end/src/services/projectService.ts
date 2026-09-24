d73b04555d6afca7999cf7bdeebe1565a88a21b9
Alexandra Chanteloup <275184320+AlexCh79@users.noreply.github.com>
2026-09-24 15:47:49 +0200

    Ajout des premiers calls projects

--- front-end/src/services/projectService.ts
+++ front-end/src/services/projectService.ts
@@ -1,6 +1,8 @@
 import { apiFetch } from "./api";
 import type { Project } from "@/types/Project";
+import type { Task } from "@/types/Task";
 
+// Création d'un nouveau projet
 export async function createProject(data: {
   name: string;
   description?: string;
@@ -12,4 +14,22 @@ export async function createProject(data: {
   });
 
   return body.data.project;
+}
+
+// Récupération de tous les projets de l'utilisateur connecté
+export async function getProjects(): Promise<Project[]> {
+  const body = await apiFetch("/projects", {
+    method: "GET",
+  });
+
+  return body.data.projects;
+}
+
+// Récupération de toutes les tâches d'un projet
+export async function getProjectTasks(id: string): Promise<Task[]> {
+  const body = await apiFetch(`/projects/${id}/tasks`, {
+    method: "GET",
+  });
+
+  return body.data.tasks;
 }
